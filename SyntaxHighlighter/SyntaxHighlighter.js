81d329cb2772e0a0c07ed3f718664c19b4302a6c
JongMan Koo <jongman@gmail.com>
2010-07-31 01:54:14 -0500

    fixed replacing br tags: doesn't crash when there's none

--- SyntaxHighlighter/SyntaxHighlighter.js
+++ SyntaxHighlighter/SyntaxHighlighter.js
@@ -2,7 +2,7 @@ $(function() {
     $("code").each(function() {
     var $this = $(this);
         if($this.attr("lang") !== undefined) {
-            $this.children("br").replaceWith("\n");
+            $this.children("br").each(function() { $(this).replaceWith("\n"); });
             var raw = $this.html();
             var lang = $this.attr("lang");
             $this.replaceWith('<pre class="brush: ' + lang + '">' + raw + '</pre>');
