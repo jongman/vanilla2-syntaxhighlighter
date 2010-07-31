d8a00aa29b4a2b64bbb80d2beffba552e3bbe72c
JongMan Koo <jongman@gmail.com>
2010-07-31 01:01:21 -0500

    List of languages are declared separately

--- SyntaxHighlighter/default.php
+++ SyntaxHighlighter/default.php
@@ -10,21 +10,19 @@ $PluginInfo['SyntaxHighlighter'] = array(
 
 class SyntaxHighlighterPlugIn extends Gdn_Plugin {
 
-    public function Base_Render_Before(&$Sender) {
-        $Sender->AddJsFile($this->GetResource("vendor" . DS . "scripts" . DS . "shCore.js",
-            FALSE, FALSE));
-        $Sender->AddJsFile($this->GetResource("vendor" . DS . "scripts" . DS . "shAutoLoader.js",
-            FALSE, FALSE));
+    protected $Brushes = Array("Python", "Cpp", "Java", "Css");
 
-        $Sender->Head->AddCss($this->GetResource("vendor" . DS . "styles" . DS . "shCore.css",
-            FALSE, FALSE));
-        $Sender->Head->AddCss($this->GetResource("vendor" . DS . "styles" . DS . "shThemeDefault.css",
-            FALSE, FALSE));
+    public function Base_Render_Before(&$Sender) {
+
+        $Sender->AddJsFile($this->GetResource("vendor" . DS . "scripts" . DS . "shCore.js", FALSE, FALSE));
+        $Sender->Head->AddCss($this->GetResource("vendor" . DS . "styles" . DS . "shCore.css", FALSE, FALSE));
+        $Sender->Head->AddCss($this->GetResource("vendor" . DS . "styles" . DS . "shThemeDefault.css", FALSE, FALSE));
         $Sender->AddJsFile($this->GetResource("SyntaxHighlighter.js", FALSE, FALSE));
         $Sender->Head->AddCss($this->GetResource("SyntaxHighlighter.css", FALSE, FALSE));
-        $this->AddBrushes(Array("Python", "Cpp", "Java"), $Sender);
+
+        $this->AddBrushes($Sender, $this->Brushes);
     }
-    protected function AddBrushes($Languages, &$Sender) {
+    protected function AddBrushes(&$Sender, $Languages) {
         foreach($Languages as $Lang) {
             $Sender->AddJsFile($this->GetResource("vendor" . DS . "scripts" .
                 DS . "shBrush" . $Lang . ".js", FALSE, FALSE));
